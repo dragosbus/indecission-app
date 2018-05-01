455a919be69b282ad8716f680f13eb062e56739c
dragosbus <dragosbusuioc94@gmail.com>
2018-05-01 12:02:49 +0200

    counter increment

--- src/App.js
+++ src/App.js
@@ -1,18 +1,57 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <Header />
+        <Main />
+      </div>
+    );
+  }
+}
+
+class Header extends Component {
+  render() {
+    return (
+      <header className="main-header">
+        <h1>Indecission App</h1>
+      </header>
+    );
+  }
+}
+
+class Main extends Component {
+  render() {
+    return (
+      <main>
+        <Counter/>
+      </main>
+    );
+  }
+}
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0
+    };
+    this.increment = this.increment.bind(this);
+  }
+  
+  increment() {
+    this.setState({
+      counter: this.state.counter + 1
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.counter}</h1>
+        <button onClick={this.increment}>+1</button>
       </div>
     );
   }
