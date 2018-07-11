f1a0dbbd3b677f660480f925ba18c9b0a08dd617
dragosbus <dragosbusuioc94@gmail.com>
2018-07-11 11:17:53 +0200

    use sass with create react app

--- expensify/src/App.js
+++ expensify/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import './App.css';
+import './styles/css/index.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        hi
+        <p>h</p>
       </div>
     );
   }
