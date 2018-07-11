fd98c2d95541506a3ed37d17800d1a1b07be1f9f
dragosbus <dragosbusuioc94@gmail.com>
2018-07-11 11:40:54 +0200

    added 4 routes

--- expensify/src/App.js
+++ expensify/src/App.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './styles/css/index.css';
+
+const ExpenseDashboardPage = () => {
+  return <div>This is from my dashboard component</div>;
+};
+
+const AddExpensePage = () => {
+  return <div>This is from my add expense component</div>;
+};
+
+const EditExpensePage = () => {
+  return <div>This is from my edit expense component</div>;
+};
+
+const HelpPage = () => {
+  return <div>This is from my help component</div>;
+};
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <p>h</p>
+        <Router>
+          <div>
+            <Route exact path="/" component={ExpenseDashboardPage} />
+            <Route exact path="/create" component={AddExpensePage} />
+            <Route exact path="edit" component={EditExpensePage} />
+            <Route exact path="help" component={HelpPage} />
+          </div>
+        </Router>
       </div>
     );
   }
