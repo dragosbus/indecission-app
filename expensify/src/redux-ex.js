5fc519ad2998be2bb1d5ce29ded4f1c24c77bd2d
dragosbus <dragosbusuioc94@gmail.com>
2018-07-13 18:53:10 +0200

    created procedure which filter the expenses

--- expensify/src/redux-ex.js
+++ expensify/src/redux-ex.js
@@ -20,6 +20,20 @@ const demoState = {
     }
 };
 
+//Get visible expenses
+const getVisibleExpenses = (expenses, {text, sortBy, startDate, endDate}) =>{
+    return expenses.filter(()=>{
+        const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate;
+        const endDateMatch = typeof endDate !== 'number' || expense.createdAt <= endDate;
+        const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
+
+        //figure out if expenses.description has the text variable string inside of it
+
+
+        return startDateMatch && endDateMatch && textMatch;
+    });
+};
+
 //ADD_EXPENSE
 const addExpense = ({
     description = '',
