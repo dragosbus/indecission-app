2437ed927fef0ebfe2bbc8c16269aca221aab379
dragosbus <dragosbusuioc94@gmail.com>
2018-07-13 14:05:20 +0200

    init redux in expensify app

---
+++ expensify/src/redux-ex.js
@@ -0,0 +1,10 @@
+import {createStore, combineReducers} from 'redux';
+
+const demoState = {
+    expenses: [{
+        id: 'ffff',
+        description: 'January Rent',
+        note: 'This is was the final payment',
+        amount: 54500
+    }]
+};
