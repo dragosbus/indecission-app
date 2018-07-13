e11709c9e7a831047016f44d19d90133ccaa9924
dragosbus <dragosbusuioc94@gmail.com>
2018-07-13 15:15:48 +0200

    created add expense action

--- expensify/src/redux-ex.js
+++ expensify/src/redux-ex.js
@@ -1,4 +1,8 @@
-import {createStore, combineReducers} from 'redux';
+import {
+    createStore,
+    combineReducers
+} from 'redux';
+import uuid from 'uuid';
 
 const demoState = {
     expenses: [{
@@ -15,3 +19,68 @@ const demoState = {
         endDate: undefined
     }
 };
+
+//ADD_EXPENSE
+const addExpense = ({
+    description = '',
+    note = '',
+    amount = 0,
+    createdAt = 0
+} = {}) => {
+    return {
+        type: 'ADD_EXPENSE',
+        expense: {
+            id: uuid(),
+            description,
+            none,
+            amount,
+            createdAt
+        }
+    }
+};
+//REMOVE_EXPENSE
+//EDIT_EXPENSE
+//SET_TEXT_FILTER
+//SORT_BY_DATE
+//SORT_BY_AMOUNT
+//SET_START_DATE
+//SET_END_DATE
+
+//Expenses Reducer
+const expensesReducerDefaultState = [];
+
+const expensesReducer = (state = expensesReducerDefaultState, action) => {
+
+    switch (action.type) {
+        default: return state
+    }
+
+};
+
+const filtersReducerDefaultState = {
+    text: '',
+    sortBy: 'date',
+    startDate: undefined,
+    endDate: undefine
+}
+//Filters reducer
+const filtersReducer = (state = filtersReducerDefaultState, action) => {
+
+    switch (action.type) {
+        default: return state
+    }
+};
+
+//Store creation
+const store = createStore(
+    combineReducers({
+        expenses: expensesReducer,
+        filters: filtersReducer
+    })
+);
+
+store.subscribe(()=>{
+    console.log(store.getState());
+});
+
+store.dispatch(addExpense({description: 'rent', amount: 100}));
