8723c43d63987145bb84a722bb6877e0ed708541
dragosbus <dragosbusuioc94@gmail.com>
2018-07-13 18:56:12 +0200

    sort the filtered expenses

--- expensify/src/redux-ex.js
+++ expensify/src/redux-ex.js
@@ -31,6 +31,12 @@ const getVisibleExpenses = (expenses, {text, sortBy, startDate, endDate}) =>{
 
 
         return startDateMatch && endDateMatch && textMatch;
+    }).sort((a,b)=>{
+        if(sortBy === 'date') {
+            return a.createdAt < b.createdAt ? 1 : -1;
+        } else if(sortBy === 'amount') {
+            return a.amount < b.amount ? 1 : -1;
+        }
     });
 };
 
