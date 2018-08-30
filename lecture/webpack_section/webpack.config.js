056482c99105467e68d3c2db81f6072602f3e30c
dragosbus <dragosbusuioc94@gmail.com>
2018-08-30 18:05:36 +0200

    better bundler

--- lecture/webpack_section/webpack.config.js
+++ lecture/webpack_section/webpack.config.js
@@ -1,27 +1,46 @@
 const path = require('path');
+const ExtractTextPuglin = require('extract-text-webpack-plugin');
 
-module.exports = {
-    entry: './src/App.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        },{
-            test: /\.scss$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public')
+module.exports = (env) => {
+    const isProduction = env === 'production';
+    const CSSExtract = new ExtractTextPuglin('styles.css');
+
+    return {
+        entry: './src/App.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            }, {
+                test: /\.scss$/,
+                use: CSSExtract.extract({
+                    use: [{
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ]
+                })
+            }]
+        },
+        plugins: [
+            CSSExtract
+        ],
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public')
+        }
     }
 };
