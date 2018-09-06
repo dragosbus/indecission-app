971ee1ad2b68753a6da02270d844f18eee24e65c
dragosbus <dragosbusuioc94@gmail.com>
2018-09-06 15:13:02 +0200

    init checking for validity of the inputs

--- expensify/src/App.js
+++ expensify/src/App.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { connect } from 'react-redux';
 import './styles/css/index.css';
-import {bindActionCreators} from 'redux';
-import {registerUserMiddleware, loggedUserMiddleware} from '../src/actions/user';
+import { bindActionCreators } from 'redux';
+import { registerUserMiddleware, loggedUserMiddleware } from '../src/actions/user';
 import { Header } from './components/Header';
 import AddExpensePage from './components/AddExpensePage';
 import EditExpensePage from './components/EditExpensePage';
@@ -27,12 +27,19 @@ class App extends Component {
                 exact
                 path="/"
                 render={props => {
-                  return this.props.user.isSignedIn ? <ExpenseDashboardPage displayName={this.props.user.email}/> : <Login history={props.history} login={this.props.login}/>;
+                  return this.props.user.isSignedIn ? (
+                    <ExpenseDashboardPage displayName={this.props.user.email} />
+                  ) : (
+                    <Login history={props.history} login={this.props.login} />
+                  );
                 }}
               />
-              <Route path="/register" render={props=>{
-                return <Register register={this.props.register} history={props.history}/>
-              }}/>
+              <Route
+                path="/register"
+                render={props => {
+                  return <Register register={this.props.register} history={props.history} />;
+                }}
+              />
               <Route exact path="/create" component={AddExpensePage} />
               <Route path="/edit/:id" component={EditExpensePage} />
               <Route exact path="/help" component={HelpPage} />
@@ -49,10 +56,12 @@ const mapStateToProps = state => ({
   user: state.user
 });
 
-
 const mapDispatchToProps = dispatch => ({
   register: bindActionCreators(registerUserMiddleware, dispatch),
   login: bindActionCreators(loggedUserMiddleware, dispatch)
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
